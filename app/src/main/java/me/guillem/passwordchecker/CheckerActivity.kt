831f79aecd17150b6cf5eeb14847e3f071544e2c
guillem <guillempejo@gmail.com>
2021-02-10 13:09:44 +0100

    Adding UI improvements

--- app/src/main/java/me/guillem/passwordchecker/CheckerActivity.kt
+++ app/src/main/java/me/guillem/passwordchecker/CheckerActivity.kt
@@ -1,12 +1,21 @@
 package me.guillem.passwordchecker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
+import android.graphics.Color
+import android.graphics.Color.*
 import android.os.Bundle
+import android.text.InputType.*
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import me.guillem.passwordchecker.databinding.ActivityCheckerBinding
+
 
 class CheckerActivity : AppCompatActivity() {
 
@@ -23,32 +32,54 @@ class CheckerActivity : AppCompatActivity() {
         binding.passwordInput.addTextChangedListener(passwordStrengthCalculator)
 
         // Observers
-        passwordStrengthCalculator.strengthLevel.observe(this, Observer {strengthLevel ->
+        passwordStrengthCalculator.strengthLevel.observe(this, Observer { strengthLevel ->
             displayStrengthLevel(strengthLevel)
         })
-        passwordStrengthCalculator.strengthColor.observe(this, Observer {strengthColor ->
+        passwordStrengthCalculator.strengthColor.observe(this, Observer { strengthColor ->
             color = strengthColor
         })
 
-        passwordStrengthCalculator.lowerCase.observe(this, Observer {value ->
+        passwordStrengthCalculator.lowerCase.observe(this, Observer { value ->
             displayPasswordSuggestions(value, binding.lowerCaseImg, binding.lowerCaseTxt)
         })
-        passwordStrengthCalculator.upperCase.observe(this, Observer {value ->
+        passwordStrengthCalculator.upperCase.observe(this, Observer { value ->
             displayPasswordSuggestions(value, binding.upperCaseImg, binding.upperCaseTxt)
         })
-        passwordStrengthCalculator.digit.observe(this, Observer {value ->
+        passwordStrengthCalculator.digit.observe(this, Observer { value ->
             displayPasswordSuggestions(value, binding.digitImg, binding.digitTxt)
         })
-        passwordStrengthCalculator.specialChar.observe(this, Observer {value ->
+        passwordStrengthCalculator.specialChar.observe(this, Observer { value ->
             displayPasswordSuggestions(value, binding.specialCharImg, binding.specialCharTxt)
         })
+
+
+        binding.hide.setOnClickListener {
+
+            when(binding.passwordInput.inputType){
+                //HIDEN TO VISIBLE
+                129 -> {
+                    binding.passwordInput.inputType = 144
+                    binding.hide.setImageDrawable(getDrawable(R.drawable.ic_invisible))
+                }
+                //VISIBLE TO HIDE
+                144 -> {
+                    binding.passwordInput.inputType = 129
+                    binding.hide.setImageDrawable(getDrawable(R.drawable.eye_ic))
+                }
+
+            }
+
+        }
+
+
+
     }
 
     private fun displayPasswordSuggestions(value: Int, imageView: ImageView, textView: TextView) {
-        if(value == 1){
+        if (value == 1) {
             imageView.setColorFilter(ContextCompat.getColor(this, R.color.bulletproof))
             textView.setTextColor(ContextCompat.getColor(this, R.color.bulletproof))
-        }else{
+        } else {
             imageView.setColorFilter(ContextCompat.getColor(this, R.color.grey))
             textView.setTextColor(ContextCompat.getColor(this, R.color.grey))
         }
@@ -61,4 +92,23 @@ class CheckerActivity : AppCompatActivity() {
         binding.strengthLevelTxt.setTextColor(ContextCompat.getColor(this, color))
         binding.strengthLevelIndicator.setBackgroundColor(ContextCompat.getColor(this, color))
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
